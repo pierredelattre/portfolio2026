2405a19502daf0c7d627a7b151b3c071b3614dbb
Pierre <pierredltre@icloud.com>
2026-04-24 19:43:20 +0200

    smooth scroll attenué

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import Lenis from '@studio-freight/lenis'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import App from './App.vue'
@@ -8,7 +9,43 @@ import { initializeLocale } from '@/i18n'
 
 import './styles/main.css'
 
+const lenis = new Lenis({
+  duration: 1.2,
+  smooth: true,
+  smoothTouch: false,
+  lerp: 0.15,
+  easing: (t) => t
+})
+
+if (typeof window !== 'undefined') {
+  window.__lenis = lenis
+}
+
 gsap.registerPlugin(ScrollTrigger)
+
+ScrollTrigger.scrollerProxy(document.body, {
+  scrollTop(value) {
+    if (typeof value === 'number') {
+      lenis.scrollTo(value, { immediate: true })
+    } else {
+      return window.scrollY || document.documentElement.scrollTop || 0
+    }
+  },
+  getBoundingClientRect() {
+    return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight }
+  }
+})
+
+lenis.on('scroll', () => {
+  ScrollTrigger.update()
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
 
 initPageAnimations()
 initializeLocale()
